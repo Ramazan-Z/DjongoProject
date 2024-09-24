af915d7e32523af0080f2f120c74601086164c56
Ramazan-Z <Ramazan_zalilov@mail.ru>
2024-09-24 12:17:56 +0400

    setting admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BlogEntry
+
+
+@admin.register(BlogEntry)
+class BlogEntryAdmin(admin.ModelAdmin):
+    """Регистрация модели блоговой записи в админке"""
+
+    list_display = ("id", "heading", "created_at", "is_publication", "view_counter")
+    list_filter = ("is_publication",)
+    search_fields = ("heading",)
