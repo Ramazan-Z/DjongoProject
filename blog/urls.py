4dd0880f96a57b5c85d1723b570551f95f8dd778
Ramazan-Z <Ramazan_zalilov@mail.ru>
2024-09-25 21:46:21 +0400

    add controllers and urls

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,8 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path("blogentry_list/", views.BlogEntryListView.as_view(), name="blogentry_list"),
+    path("blogentry_detail/<int:pk>/", views.BlogEntryDetailView.as_view(), name="blogentry_detail"),
+    path("blogentry/new/", views.BlogEntryCreateView.as_view(), name="blogentry_new"),
+    path("blogentry/<int:pk>/edit/", views.BlogEntryUpdateView.as_view(), name="blogentry_edit"),
+    path("blogentry/<int:pk>/delete/", views.BlogEntryDeleteView.as_view(), name="blogentry_delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from blog import models
 
@@ -7,4 +9,37 @@ class BlogEntryListView(ListView):
     """Контроллер страницы со списком записей"""
 
     model = models.BlogEntry
+    context_object_name = "blogentries"
+
+
+class BlogEntryDetailView(DetailView):
+    """Контроллер страницы с деталями записи"""
+
+    model = models.BlogEntry
     context_object_name = "blogentry"
+
+
+class BlogEntryCreateView(CreateView):
+    """Контроллер страницы создания записи"""
+
+    model = models.BlogEntry
+    fields = ["heading", "content", "preview", "is_publication"]
+    template_name = "blog/blogentry_form.html"
+    success_url = reverse_lazy("blog:blogentry_list")
+
+
+class BlogEntryUpdateView(UpdateView):
+    """Контроллер страницы обновления записи"""
+
+    model = models.BlogEntry
+    fields = ["heading", "content", "preview", "is_publication"]
+    template_name = "blog/blogentry_form.html"
+    context_object_name = "blogentry"
+    success_url = reverse_lazy("blog:blogentry_list")
+
+
+class BlogEntryDeleteView(DeleteView):
+    model = models.BlogEntry
+    template_name = "blog/blogentry_confirm_delete.html"
+    context_object_name = "blogentry"
+    success_url = reverse_lazy("blog:blogentry_list")
