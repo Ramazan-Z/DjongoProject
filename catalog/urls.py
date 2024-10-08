efe6985c7ebd9610e95c4b85d54cc5961816ae51
Ramazan-Z <Ramazan_zalilov@mail.ru>
2024-10-08 21:35:04 +0400

    create CRUD

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,6 +8,9 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path("home/", views.ProductsListView.as_view(), name="home"),
-    path("contacts/", views.ContactsTemplateView.as_view(), name="contacts"),
     path("product_detail/<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"),
+    path("product_new/", views.ProductCreateView.as_view(), name="product_new"),
+    path("product_edit/<int:pk>/", views.ProductUpdateView.as_view(), name="product_edit"),
+    path("product_delete/<int:pk>/", views.ProductDeleteView.as_view(), name="product_confirm_delete"),
+    path("contacts/", views.ContactsTemplateView.as_view(), name="contacts"),
 ]
